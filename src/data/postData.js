bec8f8d62d9e8dbfaaaf81dfc653d6ec7fc83a48
pixhelp <openofmind@gmail.com>
2025-04-21 16:10:01 +0200

    style postDetails

--- src/data/postData.js
+++ src/data/postData.js
@@ -5,7 +5,9 @@ export const postData = [
         id: 1,
         slug: "assiette-sante",
         title : "L'assiette santé",
-        desc : "L’Assiette Santé, conçue par des experts en nutrition du Harvard T.H. Chan School of Public Health et les éditeurs du Harvard Health Publications, est un guide pour créer des repas sains et équilibrés, que ceux-ci soient servis dans une assiette ou dans une boîte à lunch. Affichez-en une copie sur votre réfrigérateur pour vous inciter chaque jour à élaborer des repas sains et équilibrés! ",
+        desc : `L’Assiette Santé, conçue par des experts en nutrition du Harvard T.H. Chan School of Public Health et les éditeurs du Harvard Health Publications, est un guide pour créer des repas sains et équilibrés, que ceux-ci soient servis dans une assiette ou dans une boîte à lunch.
+                Affichez-en une copie sur votre réfrigérateur pour vous inciter
+                chaque jour à élaborer des repas sains et équilibrés! `,
         img : imagesPots.assietteSanteImg,
         altTitle : "Mon image du site",
         bgcolorpost : "",
@@ -15,7 +17,13 @@ export const postData = [
         id: 2,
         title : "Les portions santé",
         slug: "portions-sante",
-        desc : "Comment évaluer la quantité de nourriture que vous avez dans votre assiette. Pas si évident, ne serait-ce que parce qu’avec nos appétits respectifs nous n’avons pas tous la même perception des quantités. Prenez l’exemple d’un repas à table avec des amis. Au menu : pâtes et viande. Vous vous servez généreusement une bonne assiette de pâtes accompagnées d’un gros steak. Dans le même temps votre voisine à l’appétit d’oiseau garnit son assiette d’un petit morceau de viande accompagné d’une cuillérée de pâtes. ",
+        desc : `Comment évaluer la quantité de nourriture que vous avez
+                dans votre assiette. Pas si évident, ne serait-ce que parce qu’avec nos appétits 
+                respectifs nous n’avons pas tous la même perception des quantités. 
+                Prenez l’exemple d’un repas à table avec des amis. Au menu : pâtes et viande.
+                Vous vous servez généreusement une bonne assiette de pâtes accompagnées d’un gros steak.
+                Dans le même temps votre voisine à l’appétit d’oiseau garnit son assiette d’un petit morceau
+                de viande accompagné d’une cuillérée de pâtes. `,
         img : imagesPots.portionsSanteImg,
         altTitle : "Mon image du site",
         link : '',
--- src/pages/PostDetails/PostDetails.js
+++ src/pages/PostDetails/PostDetails.js
@@ -7,10 +7,12 @@ const PostDetail = () => {
 
     if (!post)return <p>rien ici</p>
     return(
-        <div>
-            <h2>{post.title}</h2>
-            <img src={post.img} alt={post.altTitle}/>
-            <p>{post.desc}</p>
+        <div className="p-5 flex flex-col items-center">
+            <h2 className="text-xl text-green-400 text-bold font-semibold">{post.title}</h2>
+            <div className="flex flex-col sm:flex-row mt-1 items-center">
+                <img className="w-40 sm:w-full mr-5" src={post.img} alt={post.altTitle}/>
+                <p className="text-left">{post.desc}</p>
+            </div>
         </div>
     );
 }
