950e21677efc8e5d806c5a204ec0567c6648d6e9
pixhelp <openofmind@gmail.com>
2025-04-23 14:05:02 +0200

    page about style and text

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -2,7 +2,11 @@ import styled from "styled-components";
 
 const About = () => {
     return (
-        <h2>hello wolrd</h2>
+        <div>
+            <h2>Coucou moi cest François ou Fafa</h2>       
+            <p>Je suis un développeur web, qui commence à se passionner pour React</p>
+        </div>
+
     )
 }
 
